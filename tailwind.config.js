19442c159c55e31f3bf5205a7cbfd9bd03358d48
ka-mahda <mahdakazemi1996@gmail.com>
2023-05-29 01:54:12 +0430

    add modal of slelect sections in home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const colors=require('tailwindcss/colors');
 module.exports = {
   content: ["./src/**/*.{html,js}","./index.html"],
   theme: {
     colors:{
+      ...colors,
       'LemonChiffon': '#FEF5AC',
       'SkyBlue': '#97D2EC',
       'SlateBlue':'#5F6F94',
