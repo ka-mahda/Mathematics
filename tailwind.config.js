d146a561f86062b86f223f065931e2d6ec9879f8
ka-mahda <mahdakazemi1996@gmail.com>
2023-06-02 15:02:07 +0430

    edit homepage styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-const colors=require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ["./src/**/*.{html,js}","./index.html"],
+  content: ["./src/**/*.{html,js}", "./index.html", "./simpleCalculator.html"],
   theme: {
-    colors:{
+    colors: {
       ...colors,
-      'LemonChiffon': '#FEF5AC',
-      'SkyBlue': '#97D2EC',
-      'SlateBlue':'#5F6F94',
-      'DarkBlue':'#25316D'
+      LemonChiffon: "#FEF5AC",
+      SkyBlue: "#97D2EC",
+      SlateBlue: "#5F6F94",
+      DarkBlue: "#25316D",
     },
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+      }),
+      spacing: {
+        '128': '32rem',
+      }
+    },
   },
   corePlugins: {
     aspectRatio: false,
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/container-queries'),
-
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
   ],
-}
+};
