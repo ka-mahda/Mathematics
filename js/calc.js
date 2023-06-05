984958bcf72614d3946e222c55058843fda33202
ka-mahda <mahdakazemi1996@gmail.com>
2023-06-05 20:50:05 +0430

    update calculate function

--- js/calc.js
+++ js/calc.js
@@ -1,29 +1,26 @@
 // simple calc
 
-let result = document.getElementById('result');
-let equation = '';
+let result = document.getElementById("result");
+let equation = "";
 
-document.addEventListener('keydown', function(event) {
-  if (/\d/.test(event.key)) { // check if key is a number
+document.addEventListener("keydown", function (event) {
+  if (/\d/.test(event.key)) {
+    // check if key is a number
     calculate(event.key);
-  } else if (event.key === '.') {
-    calculate('.');
-  } else if (event.key === '+') {
-    calculate('+');
-  } else if (event.key === '-') {
-    calculate('-');
-  } else if (event.key === '*') {
-    calculate('*');
-  } else if (event.key === '/') {
-    calculate('/');
-  } else if (event.key === '(') {
-    calculate('(');
-  } else if (event.key === ')') {
-    calculate(')');
-  } else if (event.key === 'Backspace') {
+  } else if (event.key === ".") {
+    calculate(".");
+  } else if (event.key === "+") {
+    calculate("+");
+  } else if (event.key === "-") {
+    calculate("-");
+  } else if (event.key === "*") {
+    calculate("*");
+  } else if (event.key === "/") {
+    calculate("/");
+  } else if (event.key === "Backspace") {
     backspace();
-  } else if (event.key === 'Enter' || event.key === '=') {
-    calculate('=');
+  } else if (event.key === "Enter" || event.key === "=") {
+    calculate("=");
   }
 });
 
@@ -33,31 +30,37 @@ function backspace() {
 }
 
 function clearResult() {
-  result.value = '';
-  equation = '';
+  result.value = "";
+  equation = "";
 }
 
 function calculate(value) {
-  if (value === '=') {
+  if (value === "=") {
     try {
-      result.value = eval(equation);
-      equation = result.value; // update equation with the previous result
+      // evaluate the expression using eval() function
+      let resultValue = eval(equation);
+
+      // update equation with the previous result
+      equation = resultValue.toString();
+
+      // update the result value
+      result.value = equation;
     } catch (error) {
-      result.value = 'Error';
+      result.value = "Error";
     }
-  } else if (value === 'C') {
-    result.value = '';
-    equation = '';
-  } else if (value === '&#x232b;') {
+  } else if (value === "C") {
+    result.value = "";
+    equation = "";
+  } else if (value === "&#x232b;") {
     equation = equation.slice(0, -1);
     result.value = equation;
-  } else if (value === 'sqrt(') {
+  } else if (value === "sqrt(") {
     equation += value;
     result.value = equation;
-  } else if (value === '^') {
-    equation += '**';
+  } else if (value === "^") {
+    equation += "**";
     result.value = equation;
-  } else if (equation === '' && '0+-*/^=)'.includes(value)) {
+  } else if (equation === "" && "0+-*/^=)".includes(value)) {
     // do nothing if equation is empty and value is an operator or equals sign
   } else {
     equation += value;
